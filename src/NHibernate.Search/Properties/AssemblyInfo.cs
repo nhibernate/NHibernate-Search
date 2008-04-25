aa67c8a15ddc0f676c01aa373f7106b15b11d528
kailuowang <kailuowang@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-04-25 14:38:24 +0000

    Added some AssemblyInfo
    git-svn-id: https://nhcontrib.svn.sourceforge.net/svnroot/nhcontrib/trunk/src/NHibernate.Search@251 d7b3437e-3345-0410-94a8-cbd290e69f67

---
+++ src/NHibernate.Search/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NHibernate.Search")]
+[assembly: AssemblyDescription(".NET port of Hibernate.Search")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NHibernate Contrib")]
+[assembly: AssemblyProduct("NHibernate.Search.Properties")]
+[assembly: AssemblyCopyright("LGPL")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("649941bf-3340-4494-a3ba-ed567b34dc3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.1001")]
+[assembly: AssemblyFileVersion("2.0.0.1001")]
