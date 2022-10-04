098b6ed0a851aa184acb8c99b2b43422ecc820db
Alex Zaytsev <hazzik@gmail.com>
2022-10-05 00:37:37 +1300

    Run tests using SQLite

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-dotnet build src/NHibernate.Search.sln
-copy build-common/teamcity-hibernate.cfg.xml src/NHibernate.Search.Tests/hibernate.cfg.xml
-dotnet test src/NHibernate.Search.sln
+dotnet build src\NHibernate.Search.sln
+copy build-common\teamcity-hibernate.cfg.xml src\NHibernate.Search.Tests\hibernate.cfg.xml /y
+dotnet test src\NHibernate.Search.sln
